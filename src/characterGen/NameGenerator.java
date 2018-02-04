ecdc741749904ca76179d3ae3ed03e01562fd4c2
Ingo Wiarda <ingo.wiarda@dewarim.de>
2018-02-04 15:30:44 +0100

    Add list of names

--- src/characterGen/NameGenerator.java
+++ src/characterGen/NameGenerator.java
@@ -1,8 +1,23 @@
 package characterGen;
 
-public class NameGenerator {
-    public static String createName(){
+import java.util.ArrayList;
+import java.util.List;
 
-        return "Hugo";
+public class NameGenerator {
+
+    static List<String> names = new ArrayList<>();
+
+    // initialize names list
+    static {
+        names.add("Hugo");
+        names.add("Kitana");
+        names.add("Selina");
+        names.add("Trutor");
+    }
+
+    public static String createName() {
+        System.out.println("We have " + names.size() + " names.");
+        int x = (int) (Math.random() * names.size());
+        return names.get(x);
     }
 }
