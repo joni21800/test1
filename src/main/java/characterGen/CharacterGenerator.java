6f520a52f7743fd75325744aa22505ed4ec7306f
ingo.wiarda <ingo_wiarda@dewarim.de>
2018-02-17 15:00:13 +0100

    Refactoring

--- src/main/java/characterGen/CharacterGenerator.java
+++ src/main/java/characterGen/CharacterGenerator.java
@@ -4,7 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class CharacterGenerator {
-    
+
     // Feld "races" in CharacterGenerator ist eine Liste von Race-Objekten.
     // Der genaue Typ ist: ArrayList für "Race" Objekte.
     // <> ist eine Kurzform für "nimm den gleichen Typ wie vorne in der Feld-Definition" - man könnte also auch ArrayList<Race>() schreiben. 
@@ -17,37 +17,38 @@ public class CharacterGenerator {
     public static final String[] attributeNames = {"Intelligenz", "Geisteskraft", "Charisma", "Stärke", "Gewandheit", "Belastbarkeit", "Schönheit", "Geschwindigkeit"};
 
 
-
-
-    
-    public static void main(String[] args) {
+    public Race getHuman() {
         List<Attribute> humanAttributes = new ArrayList<>();
 
-
-          /*Schleife über die attributes: nehme immer das nächste Element des Arrays, stecke es in das lokale Feld attribute,
+       /*Schleife über die attributes: nehme immer das nächste Element des Arrays, stecke es in das lokale Feld attribute,
           und mache etwas damit:*/
 
-        for(String attribute : attributeNames){
-            humanAttributes.add(new Attribute(attribute, new Würfel(3,6,0)));
+        for (String attribute : attributeNames) {
+            humanAttributes.add(new Attribute(attribute, new Würfel(3, 6, 0)));
         }
 
-        Race human = new Race("Human", humanAttributes);
+        return new Race("Human", humanAttributes);
+    }
+    
+    
+
+    public static void main(String[] args) {
+
 
         //Character max = new Character("Max", human);
         //max.print();
 
-
-
+        // TODO: elf in eigene Methode
         List<Attribute> elvenAttributes = new ArrayList<>();
 
-        elvenAttributes.add(new Attribute("Intelligenz", new Würfel(3,6,1)));
-        elvenAttributes.add(new Attribute("Geisteskraft", new Würfel(2,6,0)));
-        elvenAttributes.add(new Attribute("Charisma", new Würfel(2,6,0)));
-        elvenAttributes.add(new Attribute("Stärke", new Würfel(3,6,0)));
-        elvenAttributes.add(new Attribute("Gewandheit", new Würfel(4,6,0)));
-        elvenAttributes.add(new Attribute("Belastbarkeit", new Würfel(3,6,0)));
-        elvenAttributes.add(new Attribute("Schönheit", new Würfel(5,6,0)));
-        elvenAttributes.add(new Attribute("Geschwindigkeit", new Würfel(3,6,0)));
+        elvenAttributes.add(new Attribute("Intelligenz", new Würfel(3, 6, 1)));
+        elvenAttributes.add(new Attribute("Geisteskraft", new Würfel(2, 6, 0)));
+        elvenAttributes.add(new Attribute("Charisma", new Würfel(2, 6, 0)));
+        elvenAttributes.add(new Attribute("Stärke", new Würfel(3, 6, 0)));
+        elvenAttributes.add(new Attribute("Gewandheit", new Würfel(4, 6, 0)));
+        elvenAttributes.add(new Attribute("Belastbarkeit", new Würfel(3, 6, 0)));
+        elvenAttributes.add(new Attribute("Schönheit", new Würfel(5, 6, 0)));
+        elvenAttributes.add(new Attribute("Geschwindigkeit", new Würfel(3, 6, 0)));
 
         Race elv = new Race("Elv", elvenAttributes);
 
@@ -56,20 +57,21 @@ public class CharacterGenerator {
 
         System.out.println("\n");
 
+        // TODO: Zwerg in eigene Methode
         List<Attribute> dwarvenAttributes = new ArrayList<>();
 
-        dwarvenAttributes.add(new Attribute("Intelligenz", new Würfel(3,6,0)));
-        dwarvenAttributes.add(new Attribute("Geisteskraft", new Würfel(2,6,0)));
-        dwarvenAttributes.add(new Attribute("Charisma", new Würfel(2,6,0)));
-        dwarvenAttributes.add(new Attribute("Stärke", new Würfel(4,6,6)));
-        dwarvenAttributes.add(new Attribute("Gewandheit", new Würfel(3,6,0)));
-        dwarvenAttributes.add(new Attribute("Belastbarkeit", new Würfel(4,6,0)));
-        dwarvenAttributes.add(new Attribute("Schönheit", new Würfel(2,6,2)));
-        dwarvenAttributes.add(new Attribute("Geschwindigkeit", new Würfel(3,6,0)));
+        dwarvenAttributes.add(new Attribute("Intelligenz", new Würfel(3, 6, 0)));
+        dwarvenAttributes.add(new Attribute("Geisteskraft", new Würfel(2, 6, 0)));
+        dwarvenAttributes.add(new Attribute("Charisma", new Würfel(2, 6, 0)));
+        dwarvenAttributes.add(new Attribute("Stärke", new Würfel(4, 6, 6)));
+        dwarvenAttributes.add(new Attribute("Gewandheit", new Würfel(3, 6, 0)));
+        dwarvenAttributes.add(new Attribute("Belastbarkeit", new Würfel(4, 6, 0)));
+        dwarvenAttributes.add(new Attribute("Schönheit", new Würfel(2, 6, 2)));
+        dwarvenAttributes.add(new Attribute("Geschwindigkeit", new Würfel(3, 6, 0)));
 
         Race dwarf = new Race("Dwarf", dwarvenAttributes);
 
-        PlayerCharacter Gimli = new PlayerCharacter("Gimli",dwarf );
+        PlayerCharacter Gimli = new PlayerCharacter("Gimli", dwarf);
         Gimli.print();
     }
 
