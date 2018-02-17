3bb2e7c55644d7a15c9a2eb16aa65d55febd53a8
Ingo Wiarda <ingo_wiarda@dewarim.de>
2018-02-17 14:49:01 +0100

    add elv and dwarf

--- src/main/java/characterGen/CharacterGenerator.java
+++ src/main/java/characterGen/CharacterGenerator.java
@@ -20,23 +20,59 @@ public class CharacterGenerator {
 
 
 
+
     
     public static void main(String[] args) {
         List<Attribute> humanAttributes = new ArrayList<>();
-        
-        /*
-         * Schleife über die attributes: nehme immer das nächste Element des Arrays, stecke es in das lokale Feld attribute,
-         * und mache etwas damit:
-         */
+
+
+          /*Schleife über die attributes: nehme immer das nächste Element des Arrays, stecke es in das lokale Feld attribute,
+          und mache etwas damit:*/
+
         for(String attribute : attributeNames){
             humanAttributes.add(new Attribute(attribute, new Würfel(3,6,0)));
         }
-        
+
         Race human = new Race("Human", humanAttributes);
-        
-        Character max = new Character("Max", human);
-        max.print();
-        
+
+        //Character max = new Character("Max", human);
+        //max.print();
+
+
+
+        List<Attribute> elvenAttributes = new ArrayList<>();
+
+        elvenAttributes.add(new Attribute("Intelligenz", new Würfel(3,6,1)));
+        elvenAttributes.add(new Attribute("Geisteskraft", new Würfel(2,6,0)));
+        elvenAttributes.add(new Attribute("Charisma", new Würfel(2,6,0)));
+        elvenAttributes.add(new Attribute("Stärke", new Würfel(3,6,0)));
+        elvenAttributes.add(new Attribute("Gewandheit", new Würfel(4,6,0)));
+        elvenAttributes.add(new Attribute("Belastbarkeit", new Würfel(3,6,0)));
+        elvenAttributes.add(new Attribute("Schönheit", new Würfel(5,6,0)));
+        elvenAttributes.add(new Attribute("Geschwindigkeit", new Würfel(3,6,0)));
+
+        Race elv = new Race("Elv", elvenAttributes);
+
+        Character Legolas = new Character("Legolas", elv);
+        Legolas.print();
+
+        System.out.println("\n");
+
+        List<Attribute> dwarvenAttributes = new ArrayList<>();
+
+        dwarvenAttributes.add(new Attribute("Intelligenz", new Würfel(3,6,0)));
+        dwarvenAttributes.add(new Attribute("Geisteskraft", new Würfel(2,6,0)));
+        dwarvenAttributes.add(new Attribute("Charisma", new Würfel(2,6,0)));
+        dwarvenAttributes.add(new Attribute("Stärke", new Würfel(4,6,6)));
+        dwarvenAttributes.add(new Attribute("Gewandheit", new Würfel(3,6,0)));
+        dwarvenAttributes.add(new Attribute("Belastbarkeit", new Würfel(4,6,0)));
+        dwarvenAttributes.add(new Attribute("Schönheit", new Würfel(2,6,2)));
+        dwarvenAttributes.add(new Attribute("Geschwindigkeit", new Würfel(3,6,0)));
+
+        Race dwarf = new Race("Dwarf", dwarvenAttributes);
+
+        Character Gimli = new Character("Gimli",dwarf );
+        Gimli.print();
     }
-    
+
 }
